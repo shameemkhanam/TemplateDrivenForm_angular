01db7c4936e53247949b7e2cc9772552e07221cc
unknown <shameemkhanam@gmail.com>
2023-01-03 07:13:00 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'AngularForms';
+  @ViewChild('myform') form: NgForm | undefined;
+
+  onSubmit() {
+    console.log(this.form);
+  }
 }
